fbfca4f38a6fbde810b109afbea2dc98b203acac
Adlane LADJAL <aladjal@caciis.com>
2021-05-07 14:34:33 +0200

    Updated submodule Ariazel/vendor/GLFW

--- Ariazel/src/Ariazel/Application.h
+++ Ariazel/src/Ariazel/Application.h
@@ -9,6 +9,8 @@
 
 #include "Ariazel/ImGui/ImGuiLayer.h"
 
+#include "Ariazel/Renderer/Shader.h"
+
 namespace Ariazel {
 
 	class ARIAZEL_API Application
@@ -33,6 +35,7 @@ namespace Ariazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 
 		static Application* s_Instance;
 	};
